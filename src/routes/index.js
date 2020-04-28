8ba4e7c7a84a54f35af4f857be70e79eb520d9fb
Jesus Colina <jesus2007@hotmail.com>
2020-04-28 02:54:11 +0100

    Edit is working!

--- src/routes/index.js
+++ src/routes/index.js
@@ -17,6 +17,26 @@ router.post('/add', async (req, res) => {
     res.redirect('/');
 });
 
+router.get('/turn/:id', async (req, res, next) => {
+    let { id } = req.params;
+    const task = await Task.findById(id);
+    task.status = !task.status;
+    await task.save();
+    res.redirect('/');
+  });
+
+  router.get('/edit/:id', async (req, res, next) => {
+    const task = await Task.findById(req.params.id);
+    console.log(task)
+    res.render('edit', { task });
+  });
+  
+  router.post('/edit/:id', async (req, res, next) => {
+    const { id } = req.params;
+    await Task.update({_id: id}, req.body);
+    res.redirect('/');
+  });
+  
 //Delete
 router.get('/delete/:id', async (req, res, next) => {
     let { id } = req.params;
