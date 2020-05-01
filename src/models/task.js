c3bea2225bb1c5b7bea53dba78a7ec5b46aaae14
Jesus Colina <jesus2007@hotmail.com>
2020-05-01 01:23:06 +0100

    Price working

--- src/models/task.js
+++ src/models/task.js
@@ -3,7 +3,10 @@ const Schema = mongoose.Schema;
 
 const TaskSchema = new Schema ({
     title: String,
+    
     description: String,
+    price: String,
+    
     status: {
         type: Boolean,
         default: false
